9815121f6388cd25f9bd96c76a9ee9de2c06c827
Tyhj <1043315346@qq.com>
2016-11-30 19:57:08 +0800

    好的，第一步接收和返回消息完成

---
+++ src/servlet/Signup.java
@@ -0,0 +1,75 @@
+package servlet;
+
+import java.io.DataOutputStream;
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import com.google.gson.JsonObject;
+
+/**
+ * Servlet implementation class Signup
+ */
+@WebServlet("/Signup")
+public class Signup extends HttpServlet {
+	String name;
+
+	public Signup() {
+		super();
+		// TODO Auto-generated constructor stub
+	}
+
+	/**
+	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doGet(HttpServletRequest req, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println(name + "doGet+被访问");
+
+	}
+
+	/**
+	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse
+	 *      response)
+	 */
+	protected void doPost(HttpServletRequest request, HttpServletResponse response)
+			throws ServletException, IOException {
+		System.out.println(name + "doPost被访问");
+
+		request.setCharacterEncoding("UTF-8");
+		response.setCharacterEncoding("UTF-8");
+		String username = request.getParameter("username");
+		String password = request.getParameter("password");
+
+		JsonObject json = new JsonObject();
+		json.addProperty("code", "1");
+		json.addProperty("name", username);
+		json.addProperty("pas", password);
+		
+		String postJson =json.toString().trim();
+		System.out.println(postJson);
+
+		PrintWriter out = response.getWriter();
+		out.write(postJson);
+		out.close();
+	}
+
+	@Override
+	public void init() throws ServletException {
+		// TODO Auto-generated method stub
+		super.init();
+		name = getClass().getName() + "：";
+	}
+
+	@Override
+	public void destroy() {
+		// TODO Auto-generated method stub
+		super.destroy();
+	}
+}
