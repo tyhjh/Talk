2b9f7f6170f08af2ae0e0cafcae1ec2b6ea03dec
Tyhj <1043315346@qq.com>
2016-12-06 11:13:13 +0800

    上传文件完成

---
+++ src/tools/For2Mat.java
@@ -0,0 +1,44 @@
+package tools;
+
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.StringReader;
+
+public class For2Mat {
+	 //字符串转文件
+    public static boolean string2File(String res, String filePath) {
+        boolean flag = true;
+        BufferedReader bufferedReader = null;
+        BufferedWriter bufferedWriter = null;
+        try {
+            File distFile = new File(filePath);
+            if (!distFile.getParentFile().exists()) distFile.getParentFile().mkdirs();
+            bufferedReader = new BufferedReader(new StringReader(res));
+            bufferedWriter = new BufferedWriter(new FileWriter(distFile));
+            char buf[] = new char[1024];         //字符缓冲区
+            int len;
+            while ((len = bufferedReader.read(buf)) != -1) {
+                bufferedWriter.write(buf, 0, len);
+            }
+            bufferedWriter.flush();
+            bufferedReader.close();
+            bufferedWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+            flag = false;
+            return flag;
+        } finally {
+            if (bufferedReader != null) {
+                try {
+                    bufferedReader.close();
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+        return flag;
+    }
+}
